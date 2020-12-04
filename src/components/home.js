46b41af2b80987cb13cbc494ecdef53e76980846
sreenivas.hatwar <sreenivas.hatwar@accionlabs.com>
2020-12-04 18:37:32 +0530

    done the list view

--- src/components/home.js
+++ src/components/home.js
@@ -1,30 +1,51 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {View, FlatList, StyleSheet, Text, StatusBar} from 'react-native';
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 const Home = props => {
+  const renderItem = ({item}) => <Item title={item.title} />;
   return (
-    <View style={styles.container}>
-      <Text style={styles.myState}> 1223 </Text>
-    </View>
+    <FlatList
+      data={DATA}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+    />
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: 'grey',
-    height: '100%',
-    width: '100%',
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
   },
-  myState: {
-    padding: 10,
-    marginTop: 0,
-    textAlign: 'center',
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 20,
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 });
 
